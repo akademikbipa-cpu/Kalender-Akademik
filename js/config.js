cc098c5d1d5a41f53fd5cedcc3bad0e29930660b
akademikbipa-cpu <lytarybipa@gmail.com>
2026-06-11 06:27:28 +0700

    updategs+print

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  * Ganti GAS_URL dengan Web App URL milikmu setelah deploy
  */
 const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbwLwTGE7cToPfb3EQKxTn8SL3hzCqgplNkmPolWmnhA0WHzv8KDvE81FGMFvJTMjDhp/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbytUs7kPCcSAUD6b2udB9GJWOmkpUuqJmfWTArV7SbiwQkZIQnQ-QxVlrr_Hch5Hdrd/exec",
   APP_NAME: "Kalender Akademik",
   INSTITUTION: "STMIK Bina Patria Magelang",
   DEFAULT_TAHUN_AJARAN: "2025/2026",
